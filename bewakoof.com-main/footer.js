8e76106af7227dcf7b3ff5427786160cf49b3d0e
Amal Varghese <amalvarghese113112@gmail.com>
2023-03-20 07:52:44 -0700

    first commit

---
+++ bewakoof.com-main/footer.js
@@ -0,0 +1,110 @@
+var footer = () => {
+    return `
+    <main id="footer1">
+          <div class="icon">RAWAS<span>&reg;</span></div>
+          <section class="footer1-0">
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">COUSTMOR SERVICES</p>
+              <li>Contact Us</li>
+              <li>Track Order</li>
+              <li>Return Order</li>
+              <li>Cancel Order</li>
+            </ul>
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">COMPANY</p>
+              <li>About Us</li>
+              <li>We Are Hiring</li>
+              <li>Term & Condition</li>
+              <li>Privacy Policy</li>
+              <li>Blog</li>
+            </ul>
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">CONNECT WITH US</p>
+              <li>
+                <i class="fa-brands fa-facebook-square"></i>&nbsp;&nbsp; 4.7M
+                People Like This
+              </li>
+             
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">KEEP UP TO DATE</p>
+              <form>
+                <input type="text" placeholder="Enter Email Id" id="search" />
+                <input type="submit" id="submit" value="Subscribe" />
+              </form>
+            </ul>
+          </section>
+  
+          <section class="footer1-0">
+            
+  
+           
+             
+            <ul class="footer1-0-div2">
+              <p class="footer1-0-title">100% SECURE PAYMENT</p>
+              <img
+                src="https://images.bewakoof.com/web/secure-payments-image.png"
+                alt=""
+              />
+            </ul>
+          </section>
+        </main>
+  
+        <hr class="hr-row" />
+  
+        <main id="footer2">
+          <ul class="footer2-div">
+            <p class="footer2-div-title">MEN'S CLOTHING</p>
+            <li class="footer2-div-subTitle">Top Wear</li>
+            <li>Men's New Arrivals</li>
+            <li>Men's Half Sleeve T-Shirts</li>
+            <li>Men's Hoodies & Sweatshirts</li>
+            <li>Men's Long Sleeve T-shirts</li>
+            <li>Men's Printed T-shirts</li>
+            <li>Men's Plain T-shirts</li>
+            <li>Men's Vests</li>
+            <li>Men's Polo T-Shirts</li>
+            <li>Men's Kurtas</li>
+            <li>Men's Combo T-Shirts</li>
+            <li>Men's Shirts</li>
+            <li>Men's Nightwear</li>
+            <li>Men's Plus Size Store</li>
+  
+            <li class="footer2-div-subTitle">Bottom Wear</li>
+            <li>Men's Pajamas</li>
+            <li>Men's Boxer Shorts</li>
+            <li>Men's Shorts</li>
+            <li>Men's Track Pants</li>
+            <li>Men's Pants</li>
+  
+            <li class="footer2-div-subTitle">Featured</li>
+            <li>Men's Flip Flops</li>
+            <li>Men's Sliders</li>
+            <li>Marvel T-Shirts</li>
+            <li>Disney T-Shirts</li>
+            <li>Avengers T-Shirts</li>
+            <li>Star Wars T-Shirts</li>
+            <li>Batman T-Shirts</li>
+            <li>Superman T-Shirts</li>
+          </ul>
+  
+        
+  
+         
+  
+        </main>
+  
+        <hr class="hr-row" />
+  
+        <main id="footer3">
+          <h2 class="footer3-title">
+            RAWAS THE NEW AGE ONLINE SHOPPING EXPERIENCE.
+          </h2>
+         
+  
+          
+        </main>
+    `
+} 
+
+
+export default footer;
